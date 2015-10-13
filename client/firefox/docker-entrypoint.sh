d48ab211b327ea4102c3d6c9e56b1d1bf6c58475
Tito Alvarez <augustoalvarez@gmail.com>
2015-10-13 10:24:01 -0600

    Client optimizations

--- client/firefox/docker-entrypoint.sh
+++ client/firefox/docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-/opt/bin/entry_point.sh
-
+/opt/bin/entry_point.sh &
 cd /usr/src
+
+if [ -f /usr/src/scripts/entrypoint-specific.sh ]; then
+	. /usr/src/scripts/entrypoint-specific.sh
+fi;
+
+if [ -n "$GITHUB_TOKEN" ]; then
+	composer config -g github-oauth.github.com $GITHUB_TOKEN
+fi;
+
 composer install
+vendor/bin/robo run:docker-tests
+
+if [ -n "$SLACK_CHANNEL" ] && [ -n "$SLACK_TOKEN" ]; then
+	vendor/bin/robo send:codeception-output-to-slack $SLACK_CHANNEL $SLACK_TOKEN
+fi;
