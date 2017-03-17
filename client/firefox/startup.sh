3fa8855b6bacf1aa9f4ec87102aadb784c14ea76
Tito Alvarez <augustoalvarez@gmail.com>
2017-03-16 21:32:20 -0600

    New Docker client

---
+++ client/firefox/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sed -i -e "s/{SE_OPTS}/{SE_OPTS} >> \/var\/log\/selenium.log 2>\&1/g" /opt/bin/entry_point.sh
+
+/opt/bin/entry_point.sh &
+
+if [ -f /usr/src/scripts/entrypoint-specific.sh ]; then
+	. /usr/src/scripts/entrypoint-specific.sh
+fi;
+
+if [ -n "$GITHUB_TOKEN" ]; then
+	composer config -g github-oauth.github.com $GITHUB_TOKEN
+fi;
+
+cd /usr/src/tests/tests
+
+composer install --prefer-dist
+vendor/bin/robo run:container-tests
